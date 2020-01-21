2d6779956ab60c2581865eff8ae8f6c957e18974
recimo <nikola_ilic96@hotmail.com>
2020-01-21 04:00:22 +0100

    First commit

---
+++ src/main/java/SentimentResult.java
@@ -0,0 +1,29 @@
+public class SentimentResult {
+    private String sentimentType;
+    private int sentimentScore;
+    private NLPModel sentimentClass;
+
+    public String getSentimentType() {
+        return sentimentType;
+    }
+
+    public void setSentimentType(String sentimentType) {
+        this.sentimentType = sentimentType;
+    }
+
+    public int getSentimentScore() {
+        return sentimentScore;
+    }
+
+    public void setSentimentScore(int sentimentScore) {
+        this.sentimentScore = sentimentScore;
+    }
+
+    public NLPModel getSentimentClass() {
+        return sentimentClass;
+    }
+
+    public void setSentimentClass(NLPModel sentimentClass) {
+        this.sentimentClass = sentimentClass;
+    }
+}
